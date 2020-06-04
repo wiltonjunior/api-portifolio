d51884ed7fc23a2f623be50fa8aa20ff0a401146
Wilton Júnior <wiltonjunior2010@gmail.com>
2020-06-03 23:00:52 -0300

    fixed: index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 'use strict';
 
-require('domain').config();
-
+require('dotenv').config();
 const express = require('express');
 
-const app = express();
+class App {
+  constructor() {
+    this.httpPort = process.env.HTTP_PORT;
+    this.express = express();
+    this.cors = require('cors');
+    this.logger = require('./commons/logger/logger');
+    this.errorHandler = require('./commons/handler/error.handler');
 
-app.listen(process.env.PORT, () => {
-console.log('App listening port 3000')
-})
+    this.middlewares();
+    this.routes();
+    this.initialize();
+  }
 
+  middlewares() {
+    this.express.use(this.cors());
+    this.express.use(express.json());
+  }
+
+  routes() {
+    const Routes = require('./api/routes');
+    Routes.initialize(this.express);
+    this.express.use('*', this.errorHandler);
+  }
+
+  async initialize() {
+    try {
+      this.logger.info('Starting API server');
+      this.express.listen(this.httpPort);
+      this.logger.info('APP has started | port:', this.httpPort);
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+}
+
+module.exports = new App();
